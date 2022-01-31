5e9bde1ce4c35bb4866a16c60e3c551daf7c10ce
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-31 16:14:51 +0100

    Added section contact + random Pic from Vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('JS OK!')
+console.log('Vue OK!', Vue);
+
+Vue.config.devtools = true;
+
+const root = new Vue({
+   el: '#root',
+   data: {
+      user: {
+         name: 'Matteo Imbimbo',
+         avatar: '_io'
+         },
+      contacts: [
+      {
+         name: 'Michele',
+         avatar: '_1',
+      },
+      {
+         name: 'Fabio',
+         avatar: '_2',
+      },
+      {
+         name: 'Samuele',
+         avatar: '_3',
+      },
+      {
+         name: 'Luisa',
+         avatar: '_4',
+      },
+      ]
+   }
+})
